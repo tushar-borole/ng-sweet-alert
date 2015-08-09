8adb5a8fb6669e4fa848a0083c6d780d87c52b80
Matan Narkiss <matan.narkiss@bluevine.com>
2015-08-09 14:39:40 +0300

    Added promises  #2
    Now this plug-in can be used from within the JS code and not just from HTML attributes, using AngularJS promises.

--- ng-sweet-alert.js
+++ ng-sweet-alert.js
@@ -12,14 +12,15 @@
     'use strict';
     
     
-	// Check we have sweet alert js included
-	if (angular.isUndefined(window.swal)) {
-		throw "Please inlcude sweet alert js and css from http://t4t5.github.io/sweetalert/";
-	}
+    // Check we have sweet alert js included
+    if (angular.isUndefined(window.swal)) {
+        throw "Please inlcude sweet alert js and css from http://t4t5.github.io/sweetalert/";
+    }
 
     angular
         .module('ng-sweet-alert',[])
-        .directive('sweetalert', sweetalert);
+        .directive('sweetalert', sweetalert)
+        .factory('SweetAlert', sweetalert_service);
 
     sweetalert.$inject = ['$parse'];
 
@@ -56,6 +57,57 @@
             })
 
         }
+    }
+
+    // Use SweetAlert as service
+    //
+    // swal() gets two arguments;
+    // first argument is parameters Objects (with default values).
+    // second argument is Callback function when clicking on "OK"/"Cancel", which is a promise.
+    // register to the promise (using 'then') and handle the resolve / reject according to your business logic.
+    //
+    // Add 'SweetAlert' to your directive / controller / ect)
+    // Use SweetAlert.confirm(msg, options) / SweetAlert.alert(msg, options) / SweetAlert.info(msg, options) / SweetAlert.success(msg, options)
+    // pass arguments:
+    // msg; String - The message to be displayed in the alert / confirm box (mandatory).
+    // options; Object (optinal):
+    //   title: String - the title of the box.
+    //   type: String - "warning" / "info" / "error" / "success" / "" (empty string will not display a graphic icon).
+    //   showCancelButton: Boolean - shows the "cancel" button (true will behave like confirm dialog, false will behave like alert dialog).
+    // Use returned promise;
+    //
+    // SweetAlert.confirm("Are you sure?", {title : "Careful now!"})
+    //           .then(function(p) { do something on success },
+    //                 function(p) { do something on fail }
+    //           );
+    //
+    // SweetAlert.success("You have successfully completed our poll!", {title: "Good job!"});
+    function sweetalert_service($q) {
+        function swal_alert(message, options) {
+            return swal_confirm(message, angular.extend({ title:"Alert", text: message, type: "warning", showCancelButton : false }, options));
+        }
+        function swal_info(message, options) {
+            return swal_alert(message, angular.extend({type:"info"}, options));
+        }
+        function swal_success(message, options) {
+            return swal_alert(message, angular.extend({type:"success"}, options));
+        }
+        function swal_confirm(message, options) {
+            var defered = $q.defer();
+            var options = angular.extend({ title:"Alert", text: message, type: "warning", showCancelButton : true }, options);
+            swal(options, function(){
+                defered.resolve();
+            }, function(){
+                defered.reject();
+            });
+            return defered.promise;
+        }
+        return {
+            alert: swal_alert,
+            confirm: swal_confirm,
+            info : swal_info,
+            success : swal_success
+        };
     }
 
 })();
